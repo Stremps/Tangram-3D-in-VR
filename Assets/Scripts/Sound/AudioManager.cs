6db5ce08b6c86ef41f50bba23eba480a512e27ef
Gustavo Domingues <stremps.1@gmail.com>
2025-02-24 16:53:16 -0300

    Modifying/Adding global and sfx functions

--- Assets/Scripts/Sound/AudioManager.cs
+++ Assets/Scripts/Sound/AudioManager.cs
@@ -42,6 +42,39 @@ public class AudioManager : MonoBehaviour
             musicSource.Play();
         }
     }
+
+    public void Music_PlayAttached(string name, Transform parentTransform, float pitch = 1.0f)
+    {
+        // Search the sound
+        Sound targetSound = Array.Find(musicSounds, x => x.name == name);
+
+        // If not found, send erro message
+        if( targetSound == null)
+        {
+            Debug.Log($"[Music Error] - Sound '{name}' not found!");
+        }
+        // If found play the following the object
+        else
+        {
+            //Creates the game object
+            GameObject childSoundObject = new GameObject("TempMusic");
+
+            // Attach as a Child
+            childSoundObject.transform.SetParent(parentTransform);
+            childSoundObject.transform.localPosition = UnityEngine.Vector3.zero;
+
+            // Add AudioSource in the temp child game object
+            AudioSource audioSource = childSoundObject.AddComponent<AudioSource>();
+            audioSource.clip = targetSound.clip;
+            audioSource.pitch = pitch;
+            audioSource.volume = musicSource.volume;
+            audioSource.Play();
+
+            // After the clip lenght, destroy de object
+            Destroy(childSoundObject, targetSound.clip.length/pitch);
+        }
+        
+    }
     
     public void SFX_PlayGlobal(string name)
     {
@@ -57,7 +90,7 @@ public class AudioManager : MonoBehaviour
         }
     }
 
-    public void SFX_PlayAtSource(string name, UnityEngine.Vector3 position)
+    public void SFX_PlayAtSource(string name, UnityEngine.Vector3 position, float pitch = 1.0f)
     {
         // Search the soundFX
         Sound targetSound = Array.Find(sfxSounds, x => x.name == name);
@@ -70,17 +103,52 @@ public class AudioManager : MonoBehaviour
         // If found play the song at the transform of the object
         else
         {
+            // Creates the game object
+            GameObject soundObject = new GameObject("TempSoundFX");
+            soundObject.transform.position = position;
+        
+            // Add AudioSource in the temp game object
+            AudioSource audioSource = soundObject.AddComponent<AudioSource>();
+            audioSource.clip = targetSound.clip;
+            audioSource.pitch = pitch;
+            audioSource.volume = sfxSource.volume;
+            audioSource.Play();
 
-            AudioSource.PlayClipAtPoint(targetSound.clip, position, sfxSource.volume);
+            // After the clip lenght, destroy the object
+            Destroy(soundObject, targetSound.clip.length/pitch);
         }
     }
 
-    public void SFX_PlayAttached(string name, GameObject gameObject)
+    public void SFX_PlayAttached(string name, Transform parentTransform, float pitch = 1.0f)
     {
         // Search the sound
+        Sound targetSound = Array.Find(sfxSounds, x => x.name == name);
 
         // If not found, send erro message
-
+        if( targetSound == null)
+        {
+            Debug.Log($"[SFX Erro] - Sound '{name}' not found!");
+        }
         // If found play the following the object
+        else
+        {
+            //Creates the game object
+            GameObject childSoundObject = new GameObject("TempSoundFX");
+
+            // Attach as a Child
+            childSoundObject.transform.SetParent(parentTransform);
+            childSoundObject.transform.localPosition = UnityEngine.Vector3.zero;
+
+            // Add AudioSource in the temp child game object
+            AudioSource audioSource = childSoundObject.AddComponent<AudioSource>();
+            audioSource.clip = targetSound.clip;
+            audioSource.pitch = pitch;
+            audioSource.volume = sfxSource.volume;
+            audioSource.Play();
+
+            // After the clip lenght, destroy de object
+            Destroy(childSoundObject, targetSound.clip.length/pitch);
+        }
+        
     }
 }
